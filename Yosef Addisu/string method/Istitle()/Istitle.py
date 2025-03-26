00cc8dbd3f6b3331cf3fdf514ad8783c602568b3
Yosef0011 <160311578+Yosef0011@users.noreply.github.com>
2025-03-26 16:26:16 +0300

    Create Istitle.py

---
+++ Yosef Addisu/string method/Istitle()/Istitle.py
@@ -0,0 +1,7 @@
+text1 = "Hello World"
+text2 = "hello world"
+text3 = "Hello world"
+
+print(text1.istitle())  # Output: True
+print(text2.istitle())  # Output: False
+print(text3.istitle())  # Output: False
